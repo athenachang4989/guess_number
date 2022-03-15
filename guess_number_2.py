4c7b5020fdd3defb5c7201dbfa15ba778a532e85
Athena Chang <j8687julie28@gmail.com>
2022-03-15 21:15:17 +0800

    final version

---
+++ guess_number_2.py
@@ -0,0 +1,20 @@
+import random
+start = input('請決定隨機數字範圍的起始值：')
+end = input('請決定隨機數字範圍的結束值：')
+start = int(start)
+end = int(end)
+r = random. randint(start,end)
+count = 0
+while True:
+	count += 1 # count = count + 1
+	num = input('請猜數字：')
+	num = int(num)
+	if num == r:
+		print('猜中了！')
+		print('這是你猜的第', count, '次')
+		break
+	elif num > r:
+		print('小一點')
+	elif num < r:
+		print('大一點')
+	print('這是你猜的第', count, '次')
